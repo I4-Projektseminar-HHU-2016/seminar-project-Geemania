ee05f5409090a7c9b6d69f2412287caa61f216e3
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-13 21:35:29 +0200

    language donut
    added languages as donut and edited langdist

--- py files/langdist.py
+++ py files/langdist.py
@@ -1,6 +1,6 @@
-import matplotlib as plt
 import nltk
 from nltk import FreqDist
+from bokeh.charts import Donut, show, output_file
 
 class langDist(object):
     def __init__(self):
@@ -9,10 +9,34 @@ class langDist(object):
     def dist(self, data):
 
         self.langs = []
+        self.names = []
+        self.values= []
+        self.dumpV = []
+        self.v = 0
+        self.dicts = {}
         
         for value in data.values():
             self.langs.append(value)
 
         self.freq = FreqDist(self.langs).most_common()
+        for elem in self.freq:
+            (self.key, self.val) = elem
+            if self.val < 500:
+                self.dumpV.append(self.val)
+            else:
+                self.names.append(self.key)
+                self.values.append(self.val)
+
+        for dump in self.dumpV:
+            self.v += dump
+
+        self.names.append('rest')
+        self.values.append(self.v)
         
-        return self.freq
+        self.dicts['labels'] = self.names
+        self.dicts['vals'] = self.values
+
+        self.graph = Donut(self.dicts, values='vals', label='labels', title='testing it')
+        show(self.graph)
+        
+        return
