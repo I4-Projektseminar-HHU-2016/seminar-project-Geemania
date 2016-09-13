b5e7b4b29d3ed1a5efb04be68078291a4ade5047
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-13 18:42:41 +0200

    geo-map
    geo map with fetched tweet results

--- py files/main_.py
+++ py files/main_.py
@@ -1,10 +1,15 @@
 from data_Reader import CSVDataReader
+from geo import geomapping
+
 
 if __name__ == "__main__":
         
         read_data = CSVDataReader()
 
         data = read_data.FileReader('result.csv')
+        
+        mapper = geomapping()
+        mapper.mapping(data[1], data[4])
 
 		
 
