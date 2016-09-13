c9269e72f96a7de4cef443bfc913586a82506ceb
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-13 21:35:53 +0200

    main
    langdist test

--- py files/main_.py
+++ py files/main_.py
@@ -6,11 +6,11 @@ from langdist import langDist
 if __name__ == "__main__":
         
         read_data = CSVDataReader()
+        langdist = langDist()
 
         data = read_data.FileReader('result.csv')
         
         mapper = geomapping()
         mapper.mapping(data[1], data[4])
 
-		
-
+        langdist.dist(data[1])
