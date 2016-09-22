539bcd19c6bfb4e9cf5a841566eabd98fb7b0c59
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-22 19:06:47 +0200

    twitter keys

---
+++ py files/twitter_keys.py
@@ -0,0 +1,7 @@
+# Consumer keys and access tokens, used for OAuth
+# https://apps.twitter.com
+# Erstelle Dir eine App und verwende die Keys hier
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
