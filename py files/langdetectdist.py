20a52a78fd85ce41efd9cfab4ff425f4fad4bdd1
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-22 19:54:02 +0200

    adding comments

--- py files/langdetectdist.py
+++ py files/langdetectdist.py
@@ -15,31 +15,31 @@ class langDetectDist(object):
         self.v = 0
         self.dicts = {}
         
-        for value in data.values():
+        for value in data.values():	#parse tweets and try to detect the language via langdected and name the language
             try:
                 self.langs.append(detect(value))
             except:
-                self.langs.append('undetected')
+                self.langs.append('undetected') #if detection fails name it undetected
 
-        self.freq = FreqDist(self.langs).most_common()
-        for elem in self.freq:
+        self.freq = FreqDist(self.langs).most_common() #frequency of languages and sort it
+        for elem in self.freq: #parse entries and write language and frequency value
             (self.key, self.val) = elem
-            if self.val < 500:
+            if self.val < 500: #write to rest
                 self.dumpV.append(self.val)
             else:
-                self.names.append(self.key+': '+str(self.val))
+                self.names.append(self.key+': '+str(self.val)) #write to dict for graph
                 self.values.append(self.val)
 
         for dump in self.dumpV:
             self.v += dump
 
-        self.names.append('rest: '+str(self.v))
+        self.names.append('rest: '+str(self.v)) #dictionaries for bokeh
         self.values.append(self.v)
         
         self.dicts['labels'] = self.names
         self.dicts['vals'] = self.values
 
-        self.graph = Donut(self.dicts, values='vals', label='labels', title='Langdetect Frequency Distribution')
+        self.graph = Donut(self.dicts, values='vals', label='labels', title='Langdetect Frequency Distribution') #draw donut with bokeh
         output_file("langdetect_distribution.html")
         show(self.graph)
         
