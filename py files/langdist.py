722a9c18c66f99d4cc868fac4c17676420c120ce
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-22 19:56:28 +0200

    adding comments

--- py files/langdist.py
+++ py files/langdist.py
@@ -21,7 +21,7 @@ class langDist(object):
         self.freq = FreqDist(self.langs).most_common()
         for elem in self.freq:
             (self.key, self.val) = elem
-            if self.val < 500:
+            if self.val < 500: #write to rest
                 self.dumpV.append(self.val)
             else:
                 self.names.append(self.key+': '+str(self.val))
@@ -30,13 +30,13 @@ class langDist(object):
         for dump in self.dumpV:
             self.v += dump
 
-        self.names.append('rest: '+str(self.v))
+        self.names.append('rest: '+str(self.v)) #dictionaries
         self.values.append(self.v)
         
         self.dicts['labels'] = self.names
         self.dicts['vals'] = self.values
 
-        self.graph = Donut(self.dicts, values='vals', label='labels', title='Language Frequency Distribution')
+        self.graph = Donut(self.dicts, values='vals', label='labels', title='Language Frequency Distribution') #draw donut with bokeh
         output_file("language_distribution.html")
         show(self.graph)
         
