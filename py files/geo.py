9b18d41d6bf75c49a7dbd427c1dc8e5df0f30a89
Thorsten Brückner <thorsten.brueckner@hhu.de>
2016-09-22 19:24:18 +0200

    comments added

--- py files/geo.py
+++ py files/geo.py
@@ -8,28 +8,28 @@ class geomapping():
 
     def mapping(self, language, coordinates):
 
-        self.path = path.join(path.dirname(__file__), 'geomap.png')
+        self.path = path.join(path.dirname(__file__), 'geomap.png') #geo map image
 
-        self.fig = plt.figure(figsize=(18, 4), dpi=250)
-        plt.title("Tweets zu #Legion #Warcraft und #WorldofWarcraft")
+        self.fig = plt.figure(figsize=(18, 4), dpi=200) #basemap figure details
+        plt.title("Tweets zu #Legion #Warcraft und #WorldofWarcraft") #sets the title for the results
 
-        map = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180,urcrnrlon=180, lat_ts=20, resolution='l')
-        map.bluemarble(scale=0.3)
+        map = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180,urcrnrlon=180, lat_ts=20, resolution='l') #basemap default settings
+        map.bluemarble(scale=0.3) #basemap style
 
         plt.ion()
         plt.show()
-        for elem in coordinates:
+        for elem in coordinates: #marking geo tags on map with language color
             self.x, self.y = map(coordinates[elem][0], coordinates[elem][1])
             if language[elem] == 'en':
-                map.plot(self.x, self.y, 'ro', markersize=3)
+                map.plot(self.x, self.y, 'ro', markersize=3) #red for english
             elif language[elem] == 'de':
-                map.plot(self.x, self.y, 'go', markersize=3)
+                map.plot(self.x, self.y, 'go', markersize=3) #green for german
             elif language[elem] == 'fr':
-                map.plot(self.x, self.y, 'bo', markersize=3)
+                map.plot(self.x, self.y, 'bo', markersize=3) #blue for french
             elif language[elem] == 'es':
-                map.plot(self.x, self.y, 'yo', markersize=3)
+                map.plot(self.x, self.y, 'yo', markersize=3) #yellow for spanish
             elif language[elem] == 'und':
-                map.plot(self.x, self.y, 'wo', markersize=3)
+                map.plot(self.x, self.y, 'wo', markersize=3) #white for undefined
             plt.draw()
     
         plt.ioff()
